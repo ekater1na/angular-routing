e63d807b5e03ba5bfa2b2750cb025c9d32a16291
ekater1na <ekaterina_l@tut.by>
2022-01-31 16:00:47 +0300

    feat: watch routing events

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import {PageNotFoundComponent} from "./page-not-found.component";
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', component: PageNotFoundComponent }
-    ]),
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ], {enableTracing: true}),
   ],
   exports: [RouterModule]
 })
