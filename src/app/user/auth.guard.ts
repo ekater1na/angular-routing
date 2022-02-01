1f0be17a3b80b655d08bb66d60f76a3ead9493d1
ekater1na <ekaterina_l@tut.by>
2022-02-01 18:53:57 +0300

    feat: add canLoad guard

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,12 +1,20 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
 import {Observable} from 'rxjs';
 import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private authService: AuthService,
               private router: Router) {
@@ -16,6 +24,10 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.checkLoggedIn(state.url);
+  }
+
+  canLoad(route: Route): boolean {
+    return this.checkLoggedIn(route.path)
   }
 
   checkLoggedIn(url: string): boolean {
